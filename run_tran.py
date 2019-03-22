c87896cc49e292bd4ca35740ba1396b9ae75f6e6
OoGod <1617605689@qq.com>
2019-03-22 20:48:51 +0800

    do the test

--- run_tran.py
+++ run_tran.py
@@ -10,16 +10,12 @@ def wri_con(res):
     print("翻译结果已写入文件output_tra.txt")
 
 if __name__ == "__main__":
-    b = 'test.txt'
+    b = 'machine.txt'
     a = sys.argv[1] if len(sys.argv)>1 else b
     con = del_spa(a)
     print("原文：",con)
     result = translate(con)
-<<<<<<< HEAD
     print("翻译结果：",result)
-=======
-    print("翻译结果："result)
->>>>>>> 7550ec7fc028ce5fc08cb50a8c384664c441c80c
     wri_con(result)
     add_con("output_tra.txt")
     
