79474f9d1978b5e17870689eb84d1b36ebb9b1c7
OoGod <1617605689@qq.com>
2019-03-22 17:24:58 +0800

    update translate.py

--- translate.py
+++ translate.py
@@ -2,10 +2,10 @@ from lxml import etree
 import requests
 import json
 import sys
-from js import ctx
+from exjs import ctx
 
 def translate(a):
-    tk = ctx.call(a)
+    tk = ctx.call("TL",a)
     url='https://translate.google.cn/translate_a/single?client=webapp&sl=en&tl=zh-CN&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&otf=1&ssel=0&tsel=0&kc=1&tk={}&q={}'
     url=url.format(tk,a)
     con=requests.get(url)
